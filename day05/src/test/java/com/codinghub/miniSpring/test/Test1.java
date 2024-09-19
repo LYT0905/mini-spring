6efce81cae0ece319540556a2745a251d1e242e4
a12bb <2713204748@qq.com>
2024-09-19 17:03:12 +0800

    optimize:删除无用类

--- day05/src/test/java/com/codinghub/miniSpring/test/Test1.java
+++ day05/src/test/java/com/codinghub/miniSpring/test/Test1.java
@@ -6,19 +6,24 @@ import com.codinghub.miniSpring.context.ClassPathXmlApplicationContext;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-	    AService aService;
-	    BaseService bService;
-		try {
-			aService = (AService)ctx.getBean("aservice");
-		    aService.sayHello();
-		    
-		    bService = (BaseService)ctx.getBean("baseservice");
-		    bService.sayHello();
-		} catch (BeansException e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws BeansException {
+//		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+//	    AService aService;
+//	    BaseService bService;
+//		try {
+//			aService = (AService)ctx.getBean("aservice");
+//		    aService.sayHello();
+//
+//		    bService = (BaseService)ctx.getBean("baseservice");
+//		    bService.sayHello();
+//		} catch (BeansException e) {
+//			e.printStackTrace();
+//		}
+
+		ClassPathXmlApplicationContext classPathXmlApplicationContext =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		BaseService aservice =(BaseService) classPathXmlApplicationContext.getBean("baseservice");
+		aservice.sayHello();
 	}
 
 }
